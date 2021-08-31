cde6f5c9889309b41fe7782edd2d1f2497263181
Courtney Wilson <cmwilson132@gmail.com>
2021-08-30 22:24:08 -0400

    completed boom

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,35 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+  # get '/' do
+  #   "Hello World"
+  # end
   
-  # Add routes
+  get '/messages' do
+    message = Message.all.order(:created_at)
+    message.to_json
+  end
+
+  post '/messages' do
+    message = Message.create(
+      username: params[:username],
+      body: params[:body]
+    )
+    message.to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(
+      body: params[:body]
+    )
+    message.to_json
+  end
+
+  delete '/messages/:id' do
+    deleted_message = Message.find(params[:id])
+    deleted_message.destroy
+    deleted_message.to_json
+  end
   
 end
